698aaa712993e611c113d8708fc8bb85e6d0d7cb
matíasotamendimoreno <matiasotamendi26@gmail.com>
2023-10-09 15:19:05 -0300

    EPALM 01

---
+++ EPALM 01/EPALM 01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPALM_01
+{
+    public class Cuenta
+    {
+        // Titular.
+        string titular;
+        // Cantidad;
+        float cantidad;
+        public Cuenta(string titu, float cant)
+        {
+            titular = titu;
+            cantidad = cant;
+        }
+        public Cuenta(string titu)
+        {
+            titular = titu;
+        }
+        public void Ingresar(double CantidadIntro)
+        {
+            if (CantidadIntro > 0)
+            {
+                cantidad = cantidad + Convert.ToSingle(CantidadIntro);
+                // cantidad += Convert.ToSingle(CantidadIntro);
+            }
+        }
+        public void Retirar(double CantidadIntro)
+        {
+            if (CantidadIntro > cantidad)
+            {
+                cantidad = cantidad - Convert.ToSingle(CantidadIntro);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
---
+++ punto1/punto1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto1
+{
+    public class Cuenta
+    {
+        private string titular;
+        private double cantidad;
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+        public Cuenta(string titular)
+        {
+            this.titular= titular;
+        }
+        public void Ingresar(double cantidad)
+        {
+            if (cantidad > 0) 
+            {
+                this.cantidad += cantidad;
+            }
+        }
+        public void Reitrar(double cantidad)
+        {
+            if (cantidad <= this.cantidad && cantidad > 0)
+            { 
+                this.cantidad -= cantidad;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta("| Hola mundo.");
+            Cuenta cuenta2 = new Cuenta("| Adios mundo.", 10000);
+            cuenta1.Ingresar(1000);
+            cuenta1.Ingresar(-100);
+            cuenta2.Reitrar(1000);
+            cuenta2.Reitrar(1500000);
+            cuenta2.Reitrar(-100);
+            Console.ReadKey();
+        }
+    }
+}
