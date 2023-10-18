48ad09ac92bce08f3e2a7ff531fb4cbf17720f7a
matíasotamendimoreno <matiasotamendi26@gmail.com>
2023-10-18 16:14:24 -0300

    EPALM3.1

--- EPALM 01/EPALM 02/Program.cs
+++ EPALM 01/EPALM 02/Program.cs
@@ -2,108 +2,210 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace EPALM_02
+namespace EOPAM_02
 {
     public class Persona
     {
-        const string sexoConst = "H";
+        const string sexoConst = "H";        
 
-        string nombre = "";
-        int edad = 0;
-        int dni = 0;
-        string sexo;
-        double peso = 0;
-        double altura = 0;
+        string nombre = "";                 
+        int edad = 0;                       
+        string dni;                         
+        string sexo = sexoConst;            
+        double peso = 0;                    
+        double altura = 0;                  
+        Random rnd = new Random();          
 
         public string Nombre
         {
-            get
-            {
-                return nombre;
+            get 
+            { 
+                return nombre; 
             }
+            set 
+            { 
+                nombre = value; 
+            }    
         }
+
         public int Edad
         {
-            get
-            {
-                return edad;
+            get 
+            { 
+                return edad; 
+            }
+            set 
+            { 
+                edad = value; 
             }
         }
-        public int DNI
+
+        public string DNI
         {
-            get
+            get 
             {
-                return dni;
+                return dni; 
+            }
+            set 
+            { 
+                dni = value; 
             }
         }
         public string Sexo
         {
-            get
-            {
-                return sexo;
+            get 
+            { 
+                return sexo; 
+            }
+            set 
+            { 
+                sexo = value; 
             }
         }
         public double Peso
         {
-            get
+            get 
+            { 
+                return peso; 
+            }
+            set 
             {
-                return peso;
+                peso = value; 
             }
         }
         public double Altura
         {
-            get
+            get 
             {
-                return altura;
+                return altura; 
+            }
+            set 
+            { 
+                altura = value; 
             }
         }
 
-        public Persona(string nombre, int edad, int dni, string sexo, double peso, double altura)
-        {
+
+        public Persona(string nombre, int edad, double peso, double altura)
+        {         
             this.nombre = nombre;
             this.edad = edad;
-            this.dni = dni;
-            this.sexo = sexo;
+            this.dni = generarDni();
             this.peso = peso;
             this.altura = altura;
         }
-        public Persona(string nombre, int edad, string sexo)
-        {
+
+        public Persona(string nombre, int edad)
+        {                                              
             this.nombre = nombre;
             this.edad = edad;
-            this.sexo = sexo;
+            this.dni = generarDni();
         }
-        public Persona()
-        {
 
-        }
+        public Persona() { }                                                                                
+
         public int calcularIMC()
-        {
-            double resultado = Peso / (Altura * Altura);
+        {                                                      
+            double resultado = Peso / (Altura * Altura);                                
+            int devuelta = 0;                                                           
 
-            if (resultado > 20)
+            if (resultado < 20)
             {
-
+                devuelta = -1;
             }
-            if (resultado <= 20 && resultado >= 25)
-            {
-
-            }
+            //if (resultado <= 20 && resultado >= 25) {
+            //    devuelta = 0;
+            //}
             if (resultado > 25)
             {
-
+                devuelta = 1;
             }
 
-            return 2;
+            return devuelta;
         }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
+
+        public bool esMayorDeEdad()
+        {
+            return edad > 18 ? true : false;                                            
+        }
+
+        void comprobarSexo(char sexo)
+        {
+            if (!(sexo == (char)72 || sexo == (char)77))                                
+            {                                                                           
+                this.sexo = sexoConst;                                                  
+            }                                                                           
+            else
+            {
+                this.sexo = Convert.ToString(sexo);
+            }
+        }
+
+        string generarDni()
         {
 
+            this.sexo = rnd.Next(0, 2) == 1 ? sexoConst : "M";
+            comprobarSexo(Convert.ToChar(this.sexo));                       
+
+            return Convert.ToString(rnd.Next(10000000, 100000000));         
+        }
+        
+        public string mostrarTodo()
+        {
+            return $"| Nombre: {nombre}.\n Edad: {edad}. \n DNI: {dni}.\n| Sexo: {sexo}, peso: {peso}, altura: {altura}";
+        }
+    }
+
+    internal class Program
+    {
+        static public string mensajeIMC(int numero)
+        {
+            switch (numero)
+            {
+                case -1:
+                    return "| Está por demabjo del peso normal.";
+                case 0:
+                    return "| Está en el peso ideal.";
+                case 1:
+                    return "| Debe asistir a un medico.";
+            }
+            return "Error: Numero no valido.";
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("> Nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("> Edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("> Peso en kg: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("> Altura en metros: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Persona juan = new Persona(nombre, edad, peso, altura);
+            Persona pepe = new Persona(nombre, edad);
+            Persona gabriel = new Persona();
+
+            Console.WriteLine($" Juan: {mensajeIMC(juan.calcularIMC())}");
+            Console.WriteLine($" Pepe: {mensajeIMC(pepe.calcularIMC())}");
+            Console.WriteLine($" Gabriel:{mensajeIMC(gabriel.calcularIMC())}");
+
+            Console.WriteLine($"| {juan.Nombre}: {(juan.esMayorDeEdad() ? " Es mayor de edad." : " Es menor de edad.")}");
+            Console.WriteLine($"| {pepe.Nombre}: {(pepe.esMayorDeEdad() ? " Es mayor de edad." : " Es menor de edad.")}");
+            Console.WriteLine($"| {gabriel.Nombre}: {(gabriel.esMayorDeEdad() ? " Es mayor de edad." : " Es menor de edad.")}");
+
+            Console.WriteLine($"| {juan.mostrarTodo()}");
+            Console.WriteLine($"| {pepe.mostrarTodo()}");
+            Console.WriteLine($"| {gabriel.mostrarTodo()}");
+
+            Console.ReadLine();
         }
     }
 }
+
---
+++ EPALM 01/EPALM 03/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPALM_03
+{
+    public class Password
+    {
+        int longitud;
+        string contraseña = "";
+
+        public int Longitud
+        {
+            get
+            {
+                return longitud;
+            }
+            set
+            {
+                longitud = value;
+            }
+        }
+        public string Contraseña
+        {
+            get
+            {
+                return contraseña;
+            }
+            set
+            {
+                contraseña = value;
+            }
+        }
+
+        public Password(int longitud = 8)
+        {
+            this.longitud = longitud;
+        }
+
+        public bool esFuerte()
+        {
+            int Mayusculas = 0;
+            int Minusculas = 0;
+            int Numeros = 0;
+
+            for (int i = 0; i < longitud; i++)
+            {
+                if (char.IsUpper(contraseña, i))
+                {
+                    Mayusculas++;
+                }
+                if (char.IsLower(contraseña, i))
+                {
+                    Minusculas++;
+                }
+                if (char.IsNumber(contraseña, i))
+                {
+                    Numeros++;
+                }
+            }
+
+            return (Mayusculas > 2 && Minusculas > 1 && Numeros > 5) ? true : false;
+        }
+        public string generarContraseña()
+        {
+            char[] Uppers = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] Lowers = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            
+            Random random = new Random();
+            List<char> contraseñaEnProgreso = new List<char>();
+
+            for (int i = 0;  i < contraseña; i++)
+            {
+                int generadorDeRedstone = random.Next(1, 4);
+
+                switch(generadorDeRedstone)
+                {
+                    case 1:
+                        contraseñaEnProgreso.Add(Uppers[random.Next(0, Uppers.Count())]);
+                        break;
+                    case 2:
+                        contraseñaEnProgreso.Add(Lowers[random.Next(0, Uppers.Count())]);
+                        break;
+                    case 3:
+                        contraseñaEnProgreso.Add(Convert.ToChar(Convert.ToString(0, Uppers.Count())));
+                        break;
+                }
+            }
+            return Convert.ToString(contraseñaEnProgreso.ToArray());
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
